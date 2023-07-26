8ae675bd39cd7fd2a8481a8f955f2fc564d7c6b4
Reham <reham.mu433@gmail.com>
2023-07-26 07:27:08 +0300

    edit some questions

--- Reham_Almutairi_Assignment3/src/Main.java
+++ Reham_Almutairi_Assignment3/src/Main.java
@@ -5,107 +5,107 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-//        System.out.println("Question 1: ");
-//        for (int i = 1; i <= 100; i++) {
-//
-//            if( i%3 ==0 && i%5 ==0 ){
-//                System.out.println("FizzBuzz");
-//            }else if(i%3 == 0){
-//                System.out.println("Fizz");
-//            }
-//            else if(i%5 == 0){
-//                System.out.println("Buzz");
-//            } else System.out.println(i);
-//
-//        }
-//
-//        System.out.println("_____________________________________");
-//        System.out.println("Question 2: ");
-//        System.out.println("Reverse the text");
-//        String str = "The quick brown fox";
-//        System.out.println("Before reverse : ");
-//        System.out.println(str);
-//        System.out.println("After reverse : ");
-//        for (int i = str.length() -1 ; i >= 0 ; i--){
-//            System.out.print(str.charAt(i));
-//        }
-//        System.out.println();
-//
-//        System.out.println("_____________________________________");
-//        System.out.println("Question 3: ");
-//        System.out.println("Please Enter a positive integer or 0 to quit ");
-//        int input_user = input.nextInt();
-//
-//        do{
-//            if(input_user == 0)
-//                break;
-//        // to check the positive numbers
-//        if(input_user > 0) {
-//            System.out.println("The multiplication of "+ input_user +" =");
-//            for (int i = 1; i <= 10; i++) {
-//                System.out.println(input_user+" * "+ i +" = "+ input_user * i);
-//            }
-//        }else{
-//            System.out.println("The number is a negative, try again ");
-//        }
-//
-//            System.out.println("Please Enter a Positive integer or 0 to quit ");
-//             input_user = input.nextInt();
-//
-//        }while (true);
-//
-//        System.out.println("_____________________________________");
-//        System.out.println("Question 4: ");
-//
-//        System.out.println("This part for a factorial value");
-//        System.out.println("Please enter factorial number, or 0 to quit");
-//        int factorial = input.nextInt();
-//        do{
-//            if (factorial == 0) break;
-//
-//            int result_factorial = 1;
-//            for (int i =factorial ; i > 0 ; i--){
-//                result_factorial *= i;
-//            }
-//            System.out.println("The result of factorial = "+ result_factorial);
-//            System.out.println("Please enter factorial number, or 0 to quit");
-//             factorial = input.nextInt();
-//        }while (true);
-//
-//
-//
-//        System.out.println("_____________________________________");
-//        System.out.println("Question 5: ");
-//        System.out.println("Please enter two number for power");
-//        System.out.println("Enter first number : ");
-//        int first_num = input.nextInt();
-//        System.out.println("Enter second number that will be a power : ");
-//        int second_num = input.nextInt();
-//        int power = 1;
-//
-//        for (int i = 1; i <= second_num; i++) {
-//            power *= first_num;
-//        }
-//        System.out.println("The result of power  = "+ power);
-//        System.out.println("_____________________________________");
-//        System.out.println("Question 6: ");
-//        System.out.println("Please enter the numbers to sum the even and odd integers, or 0 to quit");
-//        int num_user = input.nextInt();
-//        int even = 0;
-//        int odd  = 0;
-//        do{
-//            if(num_user == 0) break;
-//            if(num_user%2 == 0){
-//                even += num_user;
-//            }else{
-//                odd += num_user;
-//            }
-//            System.out.println("Please enter the numbers to sum the even and odd integers, or 0 to quit");
-//             num_user = input.nextInt();
-//        }while (true);
-//
-//        System.out.println("The sum of even number = "+ even+"\n" +
-//                "The sum of odd number = "+odd);
+        System.out.println("Question 1: ");
+        for (int i = 1; i <= 100; i++) {
+
+            if( i%3 ==0 && i%5 ==0 ){
+                System.out.println("FizzBuzz");
+            }else if(i%3 == 0){
+                System.out.println("Fizz");
+            }
+            else if(i%5 == 0){
+                System.out.println("Buzz");
+            } else System.out.println(i);
+
+        }
+
+        System.out.println("_____________________________________");
+        System.out.println("Question 2: ");
+        System.out.println("Reverse the text");
+        String str = "The quick brown fox";
+        System.out.println("Before reverse : ");
+        System.out.println(str);
+        System.out.println("After reverse : ");
+        for (int i = str.length() -1 ; i >= 0 ; i--){
+            System.out.print(str.charAt(i));
+        }
+        System.out.println();
+
+        System.out.println("_____________________________________");
+        System.out.println("Question 3: ");
+        System.out.println("Please Enter a positive integer or 0 to quit ");
+        int input_user = input.nextInt();
+
+        do{
+            if(input_user == 0)
+                break;
+        // to check the positive numbers
+        if(input_user > 0) {
+            System.out.println("The multiplication of "+ input_user +" =");
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(input_user+" * "+ i +" = "+ input_user * i);
+            }
+        }else{
+            System.out.println("The number is a negative, try again ");
+        }
+
+            System.out.println("Please Enter a Positive integer or 0 to quit ");
+             input_user = input.nextInt();
+
+        }while (true);
+
+        System.out.println("_____________________________________");
+        System.out.println("Question 4: ");
+
+        System.out.println("This part for a factorial value");
+        System.out.println("Please enter factorial number, or 0 to quit");
+        int factorial = input.nextInt();
+        do{
+            if (factorial == 0) break;
+
+            int result_factorial = 1;
+            for (int i =factorial ; i > 0 ; i--){
+                result_factorial *= i;
+            }
+            System.out.println("The result of factorial = "+ result_factorial);
+            System.out.println("Please enter factorial number, or 0 to quit");
+             factorial = input.nextInt();
+        }while (true);
+
+
+
+        System.out.println("_____________________________________");
+        System.out.println("Question 5: ");
+        System.out.println("Please enter two number for power");
+        System.out.println("Enter first number : ");
+        int first_num = input.nextInt();
+        System.out.println("Enter second number that will be a power : ");
+        int second_num = input.nextInt();
+        int power = 1;
+
+        for (int i = 1; i <= second_num; i++) {
+            power *= first_num;
+        }
+        System.out.println("The result of power  = "+ power);
+        System.out.println("_____________________________________");
+        System.out.println("Question 6: ");
+        System.out.println("Please enter the numbers to sum the even and odd integers, or 0 to quit");
+        int num_user = input.nextInt();
+        int even = 0;
+        int odd  = 0;
+        do{
+            if(num_user == 0) break;
+            if(num_user%2 == 0){
+                even += num_user;
+            }else{
+                odd += num_user;
+            }
+            System.out.println("Please enter the numbers to sum the even and odd integers, or 0 to quit");
+             num_user = input.nextInt();
+        }while (true);
+
+        System.out.println("The sum of even number = "+ even+"\n" +
+                "The sum of odd number = "+odd);
         System.out.println("_____________________________________");
         System.out.println("Question 7: ");
         System.out.println("We check the number is prime or not");
